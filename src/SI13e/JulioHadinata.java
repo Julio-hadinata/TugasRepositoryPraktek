e07338ba5ddf2c0a181ea3559b2b8c66d1611445
Julio-hadinata <Juliohadinata14@hmail.com>
2014-10-07 08:16:34 +0700

    versi 2.3.2

---
+++ src/SI13e/JulioHadinata.java
@@ -0,0 +1,26 @@
+
+
+
+package SI13e;
+
+
+public class JulioHadinata {
+
+    
+    public static void main(String[] args) {
+       System.out.println("Teknokrat");
+       System.out.println("SANG JUARA!!");
+                  String Nama = ("Julio Hadinata");
+                  String Kelas = ("SI13e");
+                  String NPM = ("13311047");
+                  String Alamat = ("Bandar Jaya");
+       System.out.println("Nama : "+Nama);
+       System.out.println("Kelas : "+Kelas);
+       System.out.println("NPM : "+NPM);
+       System.out.println("Alamat : "+Alamat);
+       System.out.println("Tugas Upload ke Repository Praktek");
+       
+        
+    }
+    
+}
